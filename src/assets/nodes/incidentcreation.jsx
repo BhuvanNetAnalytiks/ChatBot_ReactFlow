09ec55d554fbe2271695eea3530281e06ae3cacd
Sannidhii15 <sannidhis@netanalytiks.com>
2025-02-28 10:59:17 +0530

    Added incidentcreation node

---
+++ src/assets/nodes/incidentcreation.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Handle, Position } from '@xyflow/react';
+import NodeOptions from "./NodeOptions";
+const incident= ({ data }) => {
+    return (
+        <div style={{ padding: 10, border: '1px solid #ddd', borderRadius: 5 }}>
+            <h4>Do you want to create ticket?</h4>
+            <NodeOptions id={data.id} onOptions={data.onOptions} />
+            <Handle
+                type="target" // This handle is for incoming connections
+                position={Position.Left} // Position the handle at the top of the node
+                style={{ background: '#555' }} // Customize the handle style
+            />
+
+        </div>
+    );
+};
+
+export default incident;
