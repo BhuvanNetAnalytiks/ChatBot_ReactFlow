b2458701341c9397ca83598bc1414d284cec4979
Sannidhii15 <sannidhis@netanalytiks.com>
2025-03-11 18:38:58 +0530

    Added handle and edges

--- src/components/FlowCanvas.jsx
+++ src/components/FlowCanvas.jsx
@@ -13,37 +13,261 @@ import '@xyflow/react/dist/style.css';
 import InputNode from './NodeTypes/InputNode.jsx';
 import Authenticated from './NodeTypes/Authenticated.jsx'
 import NotAuthenticated from './NodeTypes/NotAuthenticated.jsx';
+import DepartmentClassifier from './NodeTypes/DepartmentClassifier.jsx';
+import GladMessage from './NodeTypes/GladMessage.jsx';
+import RAG from './NodeTypes/Rag.jsx';
+import Response from './NodeTypes/Response.jsx';
+import GreetingMessage from './NodeTypes/GreetingNode.jsx';
+import ticketcreation from './NodeTypes/TicketCreation.jsx';
+import LLM from './NodeTypes/LLM.jsx';
+import Prompt from './NodeTypes/prompt.jsx';
 
 const nodeTypes = {
-  inputNode: InputNode,
+  start: InputNode,
   authenticatedNode: Authenticated,
   notAuthenticatedNode: NotAuthenticated,
+  departmentClassifier: DepartmentClassifier,
+  gladMessage: GladMessage,
+  rag: RAG,
+  response: Response,
+  greetingMessage: GreetingMessage,
+  ticketcreation: ticketcreation,
+  llm: LLM,
+  prompt: Prompt,
 };
 
 const initialNodes = [
   {
     "id": "1",
-    "type": "inputNode",
+    "type": "start",
     "data": {  },
-    "position": { "x": 100, "y": 600 }
+    "position": { "x": 100, "y": 200 }
+
   },
   {
     "id": "2",
     "type": "authenticatedNode",
     "data": {},
-    "position": {"x": 300, "y": 200}
+    "position": {"x": 300, "y": 40}
   },
   {
     "id": "3",
     "type": "notAuthenticatedNode",
     "data": {},
-    "position": {"x": 300, "y": 500}
+    "position": {"x": 300, "y": 200}
+  },
+  {
+    "id": "4",
+    "type": "departmentClassifier",
+    "data": {},
+    "position": {"x": 600, "y": 350}
+  },
+
+  {
+    "id": "5",
+    "type": "greetingMessage",
+    "data": {},
+    "position": {"x": 600, "y": 40}
+  },
+
+  {
+    "id": "6",
+    "type": "rag",
+    "data": {},
+    "position": {"x": 1100, "y": 300}
+  },
+  {
+    "id": "7",
+    "type": "rag",
+    "data": {},
+    "position": {"x": 1100, "y": 400}
+  },
+  {
+    "id": "8",
+    "type": "rag",
+    "data": {},
+    "position": {"x": 1100, "y": 500},
+   
+  },
+  {
+    "id": "9",
+    "type": "gladMessage",
+    "data": {},
+    "position": { x: 2000, y: 200 },
+  },
+  {
+    "id": '10',
+    "type": 'llm',
+    "data": {},
+    "position": { x: 1600, y: 300 },
+  },
+  {
+    "id": '11',
+    "type": 'gladMessage',
+    "data": {},
+    "position": { x: 1600 , y: 200 },
+
+  },
+  {
+    "id": '12', 
+    "type": 'ticketcreation',
+    "data": {},
+    "position": { x: 2000, y: 400 },
+  },
+  {
+    "id": '13',
+    "type": 'prompt',
+    "data": {},
+    "position": { x: 300, y: 350 },
+  },
+  {
+    "id": "14",
+    "type": "greetingMessage",
+    "data": {},
+    "position": {"x": 600, "y": 200}
+  },
+  {
+    "id": "15",
+    "type": "response",
+    "data": {},
+    "position": {"x": 1300, "y": 500}
+  },
+  {
+    "id": "16",
+    "type": "response",
+    "data": {},
+    "position": {"x": 1300, "y": 400}
+  },
+  {
+    "id": "17",
+    "type": "response",
+    "data": {},
+    "position": {"x": 1300, "y": 300}
   }
 ];
 
 const FlowCanvas = () => {
   const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
-  const [edges, setEdges, onEdgesChange] = useEdgesState([]);
+  const [edges, setEdges, onEdgesChange] = useEdgesState([
+    {
+      "id" : "e1-2",
+      "source" : "1",
+      "target" : "2",
+      "label" : "Yes"
+    },
+    {
+      "id" : "e1-3",
+      "source" : "1",
+      "target" : "3",
+    },
+    {
+      "id" : "e1-13",
+      "source" : "1",
+      "target" : "13",
+    },
+    {
+      "id" : "e1-13",
+      "source" : "1",
+      "target" : "13",
+    },
+    {
+      "id" : "e2-5",
+      "source" : "2",
+      "target" : "5",
+    },
+    {
+      "id" : "e3-14",
+      "source" : "3",
+      "target" : "14",
+    },
+    {
+      "id" : "e13-4",
+      "source" : "13",
+      "target" : "4",
+    },
+    {
+      "id" : "e13-4",
+      "source" : "13",
+      "target" : "4",
+    },
+    {
+      "id" : "e4-6",
+      "source" : "4",
+      "target" : "6",
+      "label":"IT",
+    },
+    {
+      "id" : "e4-7",
+      "source" : "4",
+      "target" : "7",
+      "label":"HR"
+    },
+    {
+      "id" : "e4-8",
+      "source" : "4",
+      "target" : "8",
+      "label":"Finance"
+    },
+    {
+
+      "id" : "e6-17",
+      "source" : "6",
+      "target" : "17",
+    },
+    {
+
+      "id" : "e8-15",
+      "source" : "8",
+      "target" : "15",
+    },
+    {
+
+      "id" : "e7-16",
+      "source" : "7",
+      "target" : "16",
+    },
+    {
+
+      "id" : "e8-15",
+      "source" : "8",
+      "target" : "15",
+    },
+    {
+
+      "id" : "e17-11",
+      "source" : "17",
+      "target" : "11",
+      "label":"Yes",
+    },
+    {
+
+      "id" : "e17-10",
+      "source" : "17",
+      "target" : "10",
+      "label":"No",
+    },
+    {
+
+      "id" : "e10-12",
+      "source" : "10",
+      "target" : "12",
+      "label":"No",
+    },
+    {
+
+      "id" : "e9-12",
+      "source" : "9",
+      "target" : "12",
+      "label":"Yes",
+    },
+
+    
+
+    
+
+   
+    
+  ]);
 
   const onConnect = useCallback(
     (params) => setEdges((eds) => addEdge(params, eds)),
@@ -70,3 +294,4 @@ const FlowCanvas = () => {
 };
 
 export default FlowCanvas;
+
--- src/components/NodeTypes/InputNode.jsx
+++ src/components/NodeTypes/InputNode.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
-
+import { Handle, Position } from '@xyflow/react';
 const InputNode = ({ data }) => {
   return (
     <div style={{ padding: 10, border: '1px solid #1a192b', borderRadius: 3, background: '#fff' }}>
       <strong>Start</strong>
+      <Handle
+        type="source" // This handle is for incoming connections
+        position={Position.Right} // Position the handle at the top of the node
+        style={{ background: '#555' }} // Customize the handle style
+      />
+      <Handle
+        type="target" // This handle is for incoming connections
+        position={Position.Left} // Position the handle at the top of the node
+        style={{ background: '#555' }} // Customize the handle style
+      />
     </div>
   );
 };
